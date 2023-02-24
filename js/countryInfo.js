7f2db153b2bc2961fe1ff2e27fa4d09ec792eedb
Mirza Mohibul Hasan <mirzamohibul618@gmail.com>
2023-02-24 11:38:01 +0600

    added

--- js/countryInfo.js
+++ js/countryInfo.js
@@ -30,13 +30,26 @@ const displayCountry = countries => {
     countries.map(country => {
         const newDiv = document.createElement('div');
         newDiv.classList.add('country')
-        console.log(country)
         newDiv.innerHTML = `
     <h4>Country: ${country.name.common}</h4>
     <p>Capital: ${country.capital ? country.capital : 'No capital'}</p>
     <p>Population: ${country.population}</p>
+    <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
     `;
         countryContainer.appendChild(newDiv);
     });
+}
+const loadCountryDetail = code => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]))
+}
+const displayCountryDetails = data => {
+    const detailContainer = document.getElementById('country-detail')
+    detailContainer.innerHTML = `
+        <h3>Name: ${data.name.common} </h3>
+        <img src="${data.flags.png}">
+    `
 }
 countryLoader();
